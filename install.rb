fe34245d3c7e7374d5727abb2789646525477d07
Luke Francl <look@recursion.org>
2009-08-24 18:00:49 -0700

    create test/fixie when installing the plugin if it doesn't exist

--- install.rb
+++ install.rb
@@ -1,1 +1,8 @@
-# Install hook code here
+require 'pathname'
+require 'fileutils'
+
+fixie_dir = Pathname.new(File.dirname(__FILE__)).join('..', '..', '..', 'test', 'fixie')
+if !File.exist?(fixie_dir)
+  puts "Creating test/fixie directory"
+  FileUtils.mkdir(fixie_dir)
+end
